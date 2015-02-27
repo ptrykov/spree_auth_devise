3e73d4cf5c34e7151cf6aacc4cceaddc87b2cf53
Pavel Trykov <ptrykov@gmail.com>
2015-02-27 18:30:30 +0500

    space

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Spree::Core::Engine.add_routes do
              :controllers => { :sessions => 'spree/user_sessions',
                                :registrations => 'spree/user_registrations',
                                :passwords => 'spree/user_passwords',
-                               :confirmations => 'spree/confirmations' },
+                               :confirmations => 'spree/confirmations' }, 
              :skip => [:unlocks, :omniauth_callbacks],
              :path_names => { :sign_out => 'logout' },
              :path_prefix => :user
